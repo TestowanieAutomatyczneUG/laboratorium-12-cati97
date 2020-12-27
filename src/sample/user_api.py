ec75aa8f0c2b5a8245b3add81c3b8eb1adfb7080
Katarzyna Siwiec <katasiw97@gmail.com>
2020-12-27 22:48:50 +0100

    fixed tests without mocks - changed implementation of user_api class

--- src/sample/user_api.py
+++ src/sample/user_api.py
@@ -19,19 +19,19 @@ class UserApi:
     def get_users_only_with_two_params(self, param1, param2):  # option can be gender, name, email
         new_url = self.main_url + f"?inc={param1},{param2}"
         response = self.get_api(new_url)
-        return response.json()["results"]
+        return response.json()
 
     def get_users_without_param(self, param):  # e.g. login
         new_url = self.main_url + f"?exc={param}"
         response = self.get_api(new_url)
-        return response.json()["results"]
+        return response.json()
 
     def get_user_with_nationality(self, nationality):  # e.g. gb - great britain
         new_url = self.main_url + f"?nat={nationality.upper()}"
         response = self.get_api(new_url)
-        return response.json()["results"]
+        return response.json()
 
     def get_number_of_results(self, length):
         new_url = self.main_url + f"?results={str(length)}"
         response = self.get_api(new_url)
-        return response.json()["results"]
+        return response.json()
--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -30,23 +30,23 @@ class TestUserApi(unittest.TestCase):
 
     def test_include_only_parameter_email(self):
         result = self.temp.get_users_only_with_parameter('email')
-        assert_that(result[0]).contains_key('email')
+        assert_that(result["results"][0]).contains_key('email')
 
     def test_include_two_params(self):
         result = self.temp.get_users_only_with_two_params('gender', 'phone')
-        assert_that(result[0]).does_not_contain_key('name')
+        assert_that(result["results"][0]).does_not_contain_key('name')
 
     def test_exclude_one_param(self):
         result = self.temp.get_users_without_param('login')
-        assert_that(result[0]).does_not_contain_key('login')
+        assert_that(result["results"][0]).does_not_contain_key('login')
 
     def test_get_user_with_nationality(self):
         result = self.temp.get_user_with_nationality('gb')
-        assert_that(result[0]['nat']).is_equal_to('GB')
+        assert_that(result["results"][0]['nat']).is_equal_to('GB')
 
     def test_get_number_of_results(self):
-        results = self.temp.get_number_of_results(50)
-        assert_that(results).is_length(50)
+        result = self.temp.get_number_of_results(50)
+        assert_that(result['results']).is_length(50)
 
 
 if __name__ == "__main__":
